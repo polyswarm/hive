1711fe0d7c8a6413deb03481e6030667c4f3f805
Robert Lathrop <rl@polyswarm.io>
2018-06-18 22:00:40 -0400

    Successfully build & run docker-compose.

--- launch_hive.sh
+++ launch_hive.sh
@@ -4,7 +4,7 @@ token=$(cat ./token)
 key=$(cat ./key)
 path=$(cat ./path)
 
-echo Building SSH Hop droplet
+echo "Building SSH Hop droplet"
 ud="#! /bin/bash
 mkdir hive"
 id=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"hop-hive","region":"nyc3","size":"s-1vcpu-1gb","image":"ubuntu-16-04-x64","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data":"'"$ud"'","private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null | jq -r ".droplet.id")
@@ -15,13 +15,13 @@ fi
 
 status='waiting'
 
-echo Waiting for SSH Hop to finish activating
+echo "Waiting for SSH Hop to finish activating"
 while [ "$status" != "active" ]; do 
   sleep 60
   status=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.status")
 done
 
-echo Retrieving SSH Hop private address
+echo "Retrieving SSH Hop private address"
 address=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.networks.v4[1].ip_address")
 hop=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.networks.v4[0].ip_address")
 
@@ -62,7 +62,7 @@ apt-get update
 apt-get install iptables-persistent -y
 apt-get install git"
 
-echo Creating Docker droplet
+echo "Creating Docker droplet"
 id=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"docker-hive","region":"nyc3","size":"s-1vcpu-1gb","image":"docker","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data":"'"$userdata"'","private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null | jq -r ".droplet.id")
 
 if [ $? -ne 0 ]; then
@@ -71,7 +71,7 @@ fi
 
 status='waiting'
 
-echo Waiting for Docker to finish activating
+echo "Waiting for Docker to finish activating"
 while [ "$status" != "active" ]; do 
   sleep 60
   status=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" | jq -r ".droplet.status")
@@ -93,6 +93,7 @@ echo "Docker Private IP $address"
 echo Copying files
 scp -r -i $path ./hive root@$hop:/
 
-echo Launching docker
+echo "Launching docker"
 ssh -A -tt -i $path root@$hop scp -r /hive root@$private:/
-ssh -A -tt -i $path root@$hop ssh -tt root@$private pushd /hive && chmod 755 ./standup.sh && ./standup.sh
+ssh -A -tt -i $path root@$hop ssh -tt root@$private chmod 755 /hive/standup.sh
+ssh -A -tt -i $path root@$hop ssh -tt root@$private bash /hive/standup.sh
