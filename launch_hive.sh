e39d8b4b6226a13cee5e6f0bc1a343e9f9503a06
Robert Lathrop <rl@polyswarm.io>
2018-06-22 15:39:46 -0400

    Refactor with functions.

--- launch_hive.sh
+++ launch_hive.sh
@@ -1,112 +1,120 @@
 #! /bin/bash
 
+# Load User values
 token=$(<token)
 key=$(<key)
-path=$(<path)
-compose=$(<docker-compose-priv-testnet.yml)
+
+createDockerUserData() {
+    # $1 is address
+
+    local compose=$(<docker-compose-priv-testnet.yml)
+    # Setup Docker user data
+    userdata="#! /bin/bash
+    # Add docker rules
+    iptables -P INPUT ACCEPT
+    iptables -P OUTPUT ACCEPT
+    iptables -P FORWARD ACCEPT
+    iptables -F
+    
+    service docker restart
+    
+    # Allow SSH hop to access via ssh
+    iptables -A INPUT -i eth1 -p tcp -s $1 --dport 22 -j ACCEPT
+    
+    # Allow access to dockerd ports
+    iptables -A INPUT -p tcp --dport 2735 -j ACCEPT
+    iptables -A INPUT -p tcp --dport 2736 -j ACCEPT
+    
+    # Allow SSH hop access to polyswarmd
+    iptables -A INPUT -i eth1 -p tcp -s $1 --dport 31337 -j ACCEPT
+    iptables -A OUTPUT -o eth1 -p tcp -d $1 --sport 31337 -j ACCEPT
+    
+    # Allow ssh hop access to ipfs
+    iptables -A INPUT -i eth1 -p tcp -s $1 --dport 4001 -j ACCEPT
+    iptables -A OUTPUT -o eth1 -p tcp -d $1 --sport 4001 -j ACCEPT
+    
+    # Block all ports to docker containers from outside
+    iptables -A INPUT -i eth0 -p tcp --dport 31337 -j DROP
+    iptables -A INPUT -i eth0 -p tcp --dport 8545 -j DROP
+    iptables -A INPUT -i eth0 -p tcp --dport 4001 -j DROP
+    iptables -A INPUT -i eth0 -p tcp --dport 30303 -j DROP
+    iptables -A INPUT -i eth0 -p tcp --dport 7545 -j DROP
+    
+    # Allow access via loopback
+    iptables -A INPUT -i lo -j ACCEPT
+    
+    # Allow incoming connections that we initiated already
+    iptables -I INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+    
+    # Reset policies to drop input & forward
+    iptables -P INPUT DROP 
+    iptables -P FORWARD DROP
+    
+    apt-get update
+    apt-get install iptables-persistent build-essential -y
+    curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
+    
+    echo \\\"$compose\\\" > docker-compose-priv-testnet.yml
+    
+    docker-compose -f /docker-compose-priv-testnet.yml up -d"
+}
+
+createServer() {
+    # $1 is name
+    # $2 is size
+    # $3 is image
+    # $4 is user_data
+
+    if [ "$4" = "null" ]; then
+        droplet=$(curl -v -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"'"$1"'","region":"nyc3","size":"'"$2"'","image":"'"$3"'","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data": null,"private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null)
+    else
+        droplet=$(curl -v -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"'"$1"'","region":"nyc3","size":"'"$2"'","image":"'"$3"'","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data":"'"$4"'","private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null)
+    fi
+    echo $droplet
+
+    id=$(echo "$droplet" | jq -r ".droplet.id")
+
+    status='waiting'
+    echo "Waiting for $1:$id to finish activating"
+    while [ "$status" != "active" ]; do 
+      sleep 60
+      status=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.status")
+    done
+
+    getAddrs "$1" $id 
+}
+
+getAddrs() {
+    # $1 is name
+    # $2 is id 
+    echo "Retrieving $1 private address"
+    local droplet=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$2" 2>/dev/null)
+ 
+    public=$(echo "$droplet" | jq -r ".droplet.networks.v4[0].ip_address")
+    private=$(echo "$droplet" | jq -r ".droplet.networks.v4[1].ip_address")
+
+}
 
 echo "Building SSH Hop droplet"
-id=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"hop-hive","region":"nyc3","size":"s-1vcpu-1gb","image":"ubuntu-16-04-x64","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data":null,"private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null | jq -r ".droplet.id")
+createServer hop-hive s-1vcpu-1gb ubuntu-16-04-x64 null
 
-if [ $? -ne 0 ]; then
-    exit $?
-fi
+hop_id=$id
+hop_public=$public
+hop_private=$private
 
-status='waiting'
 
-echo "Waiting for SSH Hop to finish activating"
-while [ "$status" != "active" ]; do 
-  sleep 60
-  status=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.status")
-done
+echo "Building Docker droplet"
+createDockerUserData $hop_private
+createServer docker-hive s-4vcpu-8gb docker "$userdata"
 
-echo "Retrieving SSH Hop private address"
-address=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.networks.v4[1].ip_address")
-hop=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.networks.v4[0].ip_address")
+docker_id=$id
+docker_public=$public
+docker_private=$private
 
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-# Setup Docker user data
-userdata="#! /bin/bash
-# Add docker rules
-iptables -P INPUT ACCEPT
-iptables -P OUTPUT ACCEPT
-iptables -P FORWARD ACCEPT
-iptables -F
-
-service docker restart
-
-# Allow SSH hop to access via ssh
-iptables -A INPUT -i eth1 -p tcp -s $address --dport 22 -j ACCEPT
-
-# Allow access to dockerd ports
-iptables -A INPUT -p tcp --dport 2735 -j ACCEPT
-iptables -A INPUT -p tcp --dport 2736 -j ACCEPT
-
-# Allow SSH hop access to polyswarmd
-iptables -A INPUT -i eth1 -p tcp -s $address --dport 31337 -j ACCEPT
-iptables -A OUTPUT -o eth1 -p tcp -d $address --sport 31337 -j ACCEPT
-
-# Allow ssh hop access to ipfs
-iptables -A INPUT -i eth1 -p tcp -s $address --dport 4001 -j ACCEPT
-iptables -A OUTPUT -o eth1 -p tcp -d $address --sport 4001 -j ACCEPT
-
-# Block all ports to docker containers from outside
-iptables -A INPUT -i eth0 -p tcp --dport 31337 -j DROP
-iptables -A INPUT -i eth0 -p tcp --dport 8545 -j DROP
-iptables -A INPUT -i eth0 -p tcp --dport 4001 -j DROP
-iptables -A INPUT -i eth0 -p tcp --dport 30303 -j DROP
-iptables -A INPUT -i eth0 -p tcp --dport 7545 -j DROP
-
-# Allow access via loopback
-iptables -A INPUT -i lo -j ACCEPT
-
-# Allow incoming connections that we initiated already
-iptables -I INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-# Reset policies to drop input & forward
-iptables -P INPUT DROP 
-iptables -P FORWARD DROP
-
-apt-get update
-apt-get install iptables-persistent build-essential -y
-curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-
-echo \\\"$compose\\\" > docker-compose-priv-testnet.yml
-
-docker-compose -f /docker-compose-priv-testnet.yml up -d
-"
-
-echo "Creating Docker droplet"
-id=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"docker-hive","region":"nyc3","size":"s-4vcpu-8gb","image":"docker","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data":"'"$userdata"'","private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null | jq -r ".droplet.id")
-
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-status='waiting'
-
-echo "Waiting for Docker to finish activating"
-while [ "$status" != "active" ]; do 
-  sleep 60
-  status=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.status")
-done
-
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-private=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.networks.v4[1].ip_address")
-
-if [ $? -ne 0 ]; then
-    exit $?
-fi
-
-echo "Hop ID $id"
-echo "Hop Private IP $address"
-echo "Hop Public IP $hop"
-echo "Docker ID $id"
-echo "Docker Private IP $private"
+echo "Hop ID $hop_id"
+echo "Hop Private IP $hop_private"
+echo "Hop Public IP $hop_public"
+echo "Docker ID $docker_id"
+echo "Docker Private IP $docker_private"
+echo "Docker Public IP $docker_public"
