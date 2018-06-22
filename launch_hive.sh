76296e8edfaee15b723b5031ae41103a6776ed12
Robert Lathrop <rl@polyswarm.io>
2018-06-22 14:15:13 -0400

    Stand up PolySwarm docker containers on docker droplet.

--- launch_hive.sh
+++ launch_hive.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
-token=$(cat ./token)
-key=$(cat ./key)
-path=$(cat ./path)
+token=$(<token)
+key=$(<key)
+path=$(<path)
+compose=$(<docker-compose-priv-testnet.yml)
 
 echo "Building SSH Hop droplet"
-ud="#! /bin/bash
-mkdir hive"
-id=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"hop-hive","region":"nyc3","size":"s-1vcpu-1gb","image":"ubuntu-16-04-x64","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data":"'"$ud"'","private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null | jq -r ".droplet.id")
+id=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"hop-hive","region":"nyc3","size":"s-1vcpu-1gb","image":"ubuntu-16-04-x64","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data":null,"private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null | jq -r ".droplet.id")
 
 if [ $? -ne 0 ]; then
     exit $?
@@ -29,41 +28,60 @@ if [ $? -ne 0 ]; then
     exit $?
 fi
 
-echo "Hop ID $id"
-echo "Hop Private IP $address"
-echo "Hop Public IP $hop"
-
 # Setup Docker user data
 userdata="#! /bin/bash
+# Add docker rules
 iptables -P INPUT ACCEPT
 iptables -P OUTPUT ACCEPT
 iptables -P FORWARD ACCEPT
 iptables -F
-iptables -A INPUT -p tcp -s $address --dport 22 -i eth1 -j ACCEPT
-iptables -A INPUT -p tcp -s $address --sport 31337 -j ACCEPT
-iptables -A OUTPUT -p tcp -d $address --dport 31337 -j ACCEPT
-iptables -A INPUT -p tcp -s $address --sport 5001 -j ACCEPT
-iptables -A OUTPUT -p tcp -d $address --dport 5001 -j ACCEPT
+
+service docker restart
+
+# Allow SSH hop to access via ssh
+iptables -A INPUT -i eth1 -p tcp -s $address --dport 22 -j ACCEPT
+
+# Allow access to dockerd ports
+iptables -A INPUT -p tcp --dport 2735 -j ACCEPT
+iptables -A INPUT -p tcp --dport 2736 -j ACCEPT
+
+# Allow SSH hop access to polyswarmd
+iptables -A INPUT -i eth1 -p tcp -s $address --dport 31337 -j ACCEPT
+iptables -A OUTPUT -o eth1 -p tcp -d $address --sport 31337 -j ACCEPT
+
+# Allow ssh hop access to ipfs
+iptables -A INPUT -i eth1 -p tcp -s $address --dport 4001 -j ACCEPT
+iptables -A OUTPUT -o eth1 -p tcp -d $address --sport 4001 -j ACCEPT
+
+# Block all ports to docker containers from outside
+iptables -A INPUT -i eth0 -p tcp --dport 31337 -j DROP
+iptables -A INPUT -i eth0 -p tcp --dport 8545 -j DROP
+iptables -A INPUT -i eth0 -p tcp --dport 4001 -j DROP
+iptables -A INPUT -i eth0 -p tcp --dport 30303 -j DROP
+iptables -A INPUT -i eth0 -p tcp --dport 7545 -j DROP
+
+# Allow access via loopback
 iptables -A INPUT -i lo -j ACCEPT
+
+# Allow incoming connections that we initiated already
 iptables -I INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -P INPUT DROP
+
+# Reset policies to drop input & forward
+iptables -P INPUT DROP 
 iptables -P FORWARD DROP
 
-# Without these, docker cannot build any images
-iptables -A FORWARD -j DOCKER-USER
-iptables -A FORWARD -j DOCKER-ISOLATION
-iptables -A FORWARD -o docker0 -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -o docker0 -j DOCKER
-iptables -A FORWARD -i docker0 ! -o docker0 -j ACCEPT
-iptables -A FORWARD -i docker0 -o docker0 -j ACCEPT
-iptables -A DOCKER-ISOLATION -j RETURN
-iptables -A DOCKER-USER -j RETURN
 apt-get update
-apt-get install iptables-persistent -y
-apt-get install git"
+apt-get install iptables-persistent build-essential -y
+curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo \\\"$compose\\\" > docker-compose-priv-testnet.yml
+
+docker-compose -f /docker-compose-priv-testnet.yml up -d
+"
 
 echo "Creating Docker droplet"
-id=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"docker-hive","region":"nyc3","size":"s-1vcpu-1gb","image":"docker","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data":"'"$userdata"'","private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null | jq -r ".droplet.id")
+id=$(curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $token" -d '{"name":"docker-hive","region":"nyc3","size":"s-4vcpu-8gb","image":"docker","ssh_keys":['$key'],"backups":false,"ipv6":false,"user_data":"'"$userdata"'","private_networking":true,"volumes": null,"tags":["hive"]}' "https://api.digitalocean.com/v2/droplets" 2>/dev/null | jq -r ".droplet.id")
 
 if [ $? -ne 0 ]; then
     exit $?
@@ -74,26 +92,21 @@ status='waiting'
 echo "Waiting for Docker to finish activating"
 while [ "$status" != "active" ]; do 
   sleep 60
-  status=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" | jq -r ".droplet.status")
+  status=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.status")
 done
 
 if [ $? -ne 0 ]; then
     exit $?
 fi
 
-private=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" | jq -r ".droplet.networks.v4[1].ip_address")
+private=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $token" "https://api.digitalocean.com/v2/droplets/$id" 2>/dev/null | jq -r ".droplet.networks.v4[1].ip_address")
 
 if [ $? -ne 0 ]; then
     exit $?
 fi
 
+echo "Hop ID $id"
+echo "Hop Private IP $address"
+echo "Hop Public IP $hop"
 echo "Docker ID $id"
-echo "Docker Private IP $address"
-
-echo Copying files
-scp -r -i $path ./hive root@$hop:/
-
-echo "Launching docker"
-ssh -A -tt -i $path root@$hop scp -r /hive root@$private:/
-ssh -A -tt -i $path root@$hop ssh -tt root@$private chmod 755 /hive/standup.sh
-ssh -A -tt -i $path root@$hop ssh -tt root@$private bash /hive/standup.sh
+echo "Docker Private IP $private"
