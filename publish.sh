32e4654b30499f67c96ca7b8d5b6c4e299bf596a
Robert Lathrop <rl@polyswarm.io>
2018-06-25 23:54:08 -0400

    Wait for it uses tcp. Bootnode uses udp.

--- publish.sh
+++ publish.sh
@@ -9,5 +9,4 @@ scp -i $ssh_key -r ./geth root@$hop_public:/root/geth
 ssh -tt -i $ssh_key -A root@$hop_public scp /root/docker-compose-hive.yml root@$docker_private:/root/docker-compose-hive.yml
 ssh -tt -i $ssh_key -A root@$hop_public scp -r /root/geth root@$docker_private:/root/
 ssh -tt -i $ssh_key -A root@$hop_public ssh -tt root@$docker_private docker-compose -f /root/docker-compose-hive.yml up -d bootnode
-# TODO use wait_for_it
 ssh -tt -i $ssh_key -A root@$hop_public ssh -tt root@$docker_private docker-compose -f /root/docker-compose-hive.yml up -d geth contract
